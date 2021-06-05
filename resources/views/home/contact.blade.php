863da27fa08d30e1463de98799821fd1c6f59424
Markjim <123markjim@gmail.com>
2021-06-05 22:21:50 +0800

    updated contact

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -12,6 +12,7 @@
 <div class="container mt-5 mb-5">
     <h3>Contact</h3>
     <hr>
+    <h4><i class="fas fa-code"></i><a href="https://markjim10.github.io/">markjim10.github.io</a></h4>
     <h4><i class="fas fa-envelope"></i> mjimdomondon@gmail.com</h4>
     <h4><i class="fas fa-phone"></i> 0945 4158492</h4>
 </div>
